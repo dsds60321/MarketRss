e4c5a61ebfb7a5b7d2c9a98e1c0a81256559314e
gh.kang <gh.kang@cyrexpay.com>
2024-03-06 03:29:15 +0900

    init

---
+++ src/main/java/com/gen/marketrss/MarketRssApplication.java
@@ -0,0 +1,13 @@
+package com.gen.marketrss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketRssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketRssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gen/marketrss/MarketRssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gen.marketrss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketRssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
